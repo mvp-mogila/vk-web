c2a42a110eb93351b0d6c8f34275d197c54474b9
Artem-Mo <m0gilevskiy@yandex.ru>
2023-11-04 21:40:54 +0300

    added layout and question component

--- askme/app/views.py
+++ askme/app/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpRequest, HttpResponse 
 from django.shortcuts import render
 
-# Create your views here.
+def index_view(request):
+    questions = [
+        {
+            'id': i,
+            'title': f"Title {i}",
+            'user': f"User {i}",
+            'datetime': '01.01.2023 10:10',
+            'content': f"Some text {i} ?",
+            'rating': -i * 2,
+            'tags': [f"tag {j}" for j in range(3)]
+        } for i in range(10)
+    ]
+    context = {'questions': questions}
+    return render(request, "index.html", context)
--- askme/askme/urls.py
+++ askme/askme/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
+from app import views
+
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index_view, name = "index"),
+    path('admin/', admin.site.urls, name = "admin"),
 ]
