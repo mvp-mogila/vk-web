ab00fa83fc466fa873d0f8e330c3d8805e2e2c18
Artem-Mo <m0gilevskiy@yandex.ru>
2023-12-21 15:48:37 +0300

    Models changes

--- askme/app/models.py
+++ askme/app/models.py
@@ -7,18 +7,18 @@ from app.managers import QuestionManager, TagManager, UserManager
 
 
 class Question(models.Model):
-    title = models.CharField(max_length=50)
+    title = models.CharField(max_length=30)
     content = models.TextField()
     time_created = models.DateTimeField(auto_now=False, auto_now_add=True)
     rating = models.IntegerField(default=0)
 
-    author = models.ForeignKey("Profile", on_delete=models.PROTECT, related_name="question")
-    tags = models.ManyToManyField("Tag", related_name="question")
-    reactions = GenericRelation("Reaction")
+    author = models.ForeignKey('Profile', on_delete=models.PROTECT, related_name='question')
+    tags = models.ManyToManyField('Tag', related_name='question')
+    reactions = GenericRelation('Reaction', related_name='question')
 
     objects = QuestionManager()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.title
         
 
@@ -28,35 +28,35 @@ class Answer(models.Model):
     correct = models.BooleanField(default=False)
     rating = models.IntegerField(default=0)
 
-    author = models.ForeignKey("Profile", on_delete=models.PROTECT, related_name="answer")
-    question = models.ForeignKey("Question", on_delete=models.CASCADE, related_name="answer")
-    reactions = GenericRelation("Reaction")
+    author = models.ForeignKey('Profile', on_delete=models.PROTECT, related_name='answer')
+    question = models.ForeignKey('Question', on_delete=models.CASCADE, related_name='answer')
+    reactions = GenericRelation('Reaction', related_name='answer')
+
+    # TODO: objects = Answermanager()
 
     class Meta:
         ordering = ['-rating']
 
-    def __str__(self):
-        return self.content
-
 
 class Tag(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 20)
+    models.UniqueConstraint(name, 'name')
 
     objects = TagManager()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.name
 
 
 class Profile(models.Model):
-    profile_pic = models.ImageField(blank=True, null=True, upload_to="../uploads")
+    profile_pic = models.ImageField(blank=True, null=True, upload_to='../uploads')
     rating = models.IntegerField(default=0)
 
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     objects = UserManager()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return self.user.username
 
 
@@ -64,10 +64,13 @@ class Reaction(models.Model):
     positive = models.BooleanField()
     time_created = models.DateTimeField(auto_now=False, auto_now_add=True)
 
-    author = models.ForeignKey("Profile", on_delete=models.DO_NOTHING, related_name="reaction")
+    author = models.ForeignKey('Profile', on_delete=models.DO_NOTHING, related_name='reaction')
 
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.IntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
 
-    # objects = ReactionManager()
+    # TODO objects = ReactionManager()
+
+    def __str__(self) -> str:
+        return str(self.positive)
